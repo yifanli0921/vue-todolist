d681bbdf0889e912fdb6e31993286e1f43520536
yifan li <yifan.li@thoguhtworks.com>
2021-10-27 04:52:21 +0800

    [yifan li]feat:delete items when click button.

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,13 @@ export default {
   display: block;
   margin: 20px auto;
   height: 75px;
-
+}
+#app {
+  color: lightcoral;
+  font-size: 24px;
+  margin-top: 60px;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  font-family: Arial,sans-serif;
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="header">
     <input type="text" class="todo-input" placeholder="what needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-    <div v-for="todo in todos" :key="todo.id"
+    <div v-for="(todo,index) in todos" :key="todo.id"
     class="todo-item">
+      <div>
       {{todo.title}}
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        <button>Delete</button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,6 +46,9 @@ export default {
       })
       this.newTodo = ''
       this.idForTodo++
+    },
+    removeTodo (index) {
+      this.todos.splice(index, 1)
     }
   }
 }
@@ -59,5 +67,13 @@ export default {
   font-size: 18px;
   margin-bottom: 16px;
 }
-
+  :focus {
+    outline: 0;
+  }
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
