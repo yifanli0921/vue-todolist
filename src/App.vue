c1251a1d9fe312bf3810f979a24312bc525089e0
yifan li <yifan.li@thoguhtworks.com>
2021-10-26 20:51:47 +0800

    [yifan li]:a simple todolist.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    Todo list goes here !
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Todo List!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.header {
+  display: block;
+  text-align: center;
+}
+
+</style>
